7d63eb3fc477beaed2f51d4de02714caf5434f7c
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-11-24 20:07:22 -0500

    feat: added setTerm, setTemplate methods

--- hardhat_project/contracts/Registry.sol
+++ hardhat_project/contracts/Registry.sol
@@ -4,22 +4,24 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Registry {
+contract Registry is Ownable {
+    // string[] public templates;
     mapping(uint256 => string) public templates;
-    mapping(string => mapping(uint256 => string)) public terms;
-    // address of the user who has accepted terms for a certain agreementID (uint256)
-    mapping(address => mapping(uint256 => bool)) hasAcceptedTerms;
+    // mapping(string => mapping(uint256 => string)) public terms;
+    // maps hashAddressId to boolean
+    mapping(bytes32 => bool) hasAcceptedTerms;
     mapping(uint256 => string) public renderers;
     mapping(uint256 => uint256) public lastTermChange;
+    // mapping of hash of key and template token ID to the value
+    mapping(bytes32 => string) public terms;
+    mapping(uint256 => address) public templateOwners;
+    // address[] owners;
 
     /// @notice The default value of the global renderer.
     /// @dev The default value of the global renderer.
     string _globalRenderer = "";
-
-    /// @notice The default value of the global template.
-    /// @dev The default value of the global template.
-    string _globalDocTemplate = "";
 
     event AcceptedTerms(
         address indexed user,
@@ -27,7 +29,23 @@ contract Registry {
         string uri
     );
 
-    // mapping()
+    // change to internal later
+    function hashKeyId(string memory key, uint256 templateId)
+        public
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(templateId, key));
+    }
+
+    // change to internal later
+    function hashAddressId(address user, uint256 templateId)
+        public
+        pure
+        returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(user, templateId));
+    }
 
     function acceptedTerms(address to, uint256 tokenId)
         external
@@ -37,40 +55,50 @@ contract Registry {
         return _acceptedTerms(to, tokenId);
     }
 
-    function _acceptedTerms(address to, uint256 tokenId)
+    function _acceptedTerms(address to, uint256 templateId)
         internal
         view
         returns (bool)
     {
-        return hasAcceptedTerms[to][tokenId];
+        bytes32 hash = hashAddressId(to, templateId);
+        return hasAcceptedTerms[hash];
     }
 
     function acceptTerms(uint256 tokenId, string memory newtermsUrl) external {
         _acceptTerms(tokenId, newtermsUrl);
     }
 
-    function _acceptTerms(uint256 _tokenId, string memory _newtermsUrl)
+    function _acceptTerms(uint256 _templateId, string memory _newtermsUrl)
         internal
         virtual
     {
         require(
             keccak256(bytes(_newtermsUrl)) ==
-                keccak256(bytes(_termsUrl(_tokenId))),
+                keccak256(bytes(_termsUrl(_templateId))),
             "Terms Url does not match"
         );
-        hasAcceptedTerms[msg.sender][_tokenId] = true;
-        emit AcceptedTerms(msg.sender, _tokenId, _termsUrl(_tokenId));
+        bytes32 hash = hashAddressId(msg.sender, _templateId);
+        hasAcceptedTerms[hash] = true;
+        emit AcceptedTerms(msg.sender, _templateId, _termsUrl(_templateId));
     }
 
-    function termsUrl(uint256 tokenId) external view returns (string memory) {
-        return _termsUrlWithPrefix(tokenId, "ipfs://");
+    function termsUrl(uint256 templateId)
+        external
+        view
+        returns (string memory)
+    {
+        return _termsUrlWithPrefix(templateId, "ipfs://");
     }
 
-    function _termsUrl(uint256 tokenId) internal view returns (string memory) {
-        return _termsUrlWithPrefix(tokenId, "ipfs://");
+    function _termsUrl(uint256 templateId)
+        internal
+        view
+        returns (string memory)
+    {
+        return _termsUrlWithPrefix(templateId, "ipfs://");
     }
 
-    function _termsUrlWithPrefix(uint256 tokenId, string memory prefix)
+    function _termsUrlWithPrefix(uint256 templateId, string memory prefix)
         internal
         view
         returns (string memory _termsURL)
@@ -78,17 +106,17 @@ contract Registry {
         _termsURL = string(
             abi.encodePacked(
                 prefix,
-                _tokenRenderer(tokenId),
+                _renderer(templateId),
                 "/#/",
-                _tokenTemplate(tokenId),
+                _template(templateId),
                 "::",
                 Strings.toString(block.chainid),
                 "::",
                 Strings.toHexString(uint160(address(this)), 20),
                 "::",
-                Strings.toString(lastTermChange[tokenId]),
+                Strings.toString(lastTermChange[templateId]),
                 "::",
-                Strings.toString(tokenId)
+                Strings.toString(templateId)
             )
         );
     }
@@ -101,21 +129,51 @@ contract Registry {
         return _termsUrlWithPrefix(tokenId, prefix);
     }
 
-    function _tokenRenderer(uint256 _tokenId)
-        internal
-        view
-        returns (string memory)
-    {
+    function _renderer(uint256 _tokenId) internal view returns (string memory) {
         if (bytes(renderers[_tokenId]).length == 0) return _globalRenderer;
         else return renderers[_tokenId];
     }
 
-    function _tokenTemplate(uint256 _tokenId)
-        internal
-        view
-        returns (string memory)
-    {
-        if (bytes(templates[_tokenId]).length == 0) return _globalDocTemplate;
-        else return templates[_tokenId];
+    function _template(uint256 _tokenId) internal view returns (string memory) {
+        // if (bytes(templates[_tokenId]).length == 0) return _globalDocTemplate;
+        // else return templates[_tokenId];
+        return templates[_tokenId];
     }
+
+    function _setGlobalRenderer(string memory _newGlobalRenderer) internal {
+        _globalRenderer = _newGlobalRenderer;
+    }
+
+    function setGlobalRenderer(string memory _newGlobalRenderer)
+        external
+        onlyOwner
+    {
+        _setGlobalRenderer(_newGlobalRenderer);
+    }
+
+    function setTemplate(uint256 _templateId, string memory _newTemplate)
+        external
+        onlyTemplateOwner(_templateId)
+    {
+        templates[_templateId] = _newTemplate;
+    }
+
+    function setTerm(
+        uint256 _templateId,
+        string memory _key,
+        string memory _value
+    ) external onlyTemplateOwner(_templateId) {
+        bytes32 hash = hashKeyId(_key, _templateId);
+        terms[hash] = _value;
+    }
+
+    modifier onlyTemplateOwner(uint256 _templateId) {
+        require(
+            templateOwners[_templateId] == msg.sender,
+            "Not template owner"
+        );
+        _;
+    }
+
+    // function
 }
