a007d178996bfe8fafe2151b9369ea1644093102
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-11-30 19:51:04 -0500

    feat: added more chains

--- hardhat_project/hardhat.config.ts
+++ hardhat_project/hardhat.config.ts
@@ -1,8 +1,35 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
+// console.log(process.env);
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    polygonMumbai: process.env.ALCHEMY_MUMBAI
+      ? {
+          url: process.env.ALCHEMY_MUMBAI || "",
+          accounts: [process.env.PK || ""],
+        }
+      : undefined,
+    polygon: process.env.ALCHEMY_POLYGON
+      ? {
+          url: process.env.ALCHEMY_POLYGON || "",
+          accounts: [process.env.PK || ""],
+        }
+      : undefined,
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGON_API || "",
+      polygon: process.env.POLYGON_API || "",
+    },
+  },
+    
 };
 
 export default config;
